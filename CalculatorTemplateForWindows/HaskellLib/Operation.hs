d5eb51cfd2125d83d05acfee831a4cbf7f8013e9
Rickiarty <fatalframe0719@gmail.com>
2022-06-27 14:05:48 +0800

    updated via CLI

---
+++ CalculatorTemplateForWindows/HaskellLib/Operation.hs
@@ -0,0 +1,21 @@
+﻿
+module Operation where
+
+import Foreign.C.String
+import Foreign.C.Types
+
+foreign export ccall
+    operation :: CInt -> IO CInt
+
+operation :: CInt -> IO CInt
+operation operand = return (factorial operand) 
+
+
+foreign export ccall
+    factorial :: CInt -> CInt
+
+factorial :: CInt -> CInt
+factorial 0 = 1
+factorial num = if num > 0
+                then num * (factorial (num-1))
+                else -1
